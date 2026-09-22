b0e5010a9e65a0092431c317e1ce21e7154da26e
Dmitriy Folomkin <dmitriy.folomkin@yandex.ru>
2026-09-22 12:15:30 +0300

    Внесены правки и комментарии для чистоты кода

--- back/2026-09-22_Kafka/tasks/src/main/java/dev/folomkin/tasks/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.folomkin.tasks.repository;
-
-import dev.folomkin.tasks.entity.Tasks;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TaskRepository extends MongoRepository<Tasks, String> {
-}
--- back/2026-09-22_Kafka/users/src/main/java/dev/folomkin/users/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.folomkin.users.repository;
-
-import dev.folomkin.users.entity.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends MongoRepository<User, String> {
-}
